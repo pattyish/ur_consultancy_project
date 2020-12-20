3dca6a2bbc500431e846f4556726485c0029305b
pattyish <patrickishimwe10@gmail.com>
2020-12-20 17:22:09 +0200

    adding more clients, consultancy table

--- public/includes/layouts/header.php
+++ public/includes/layouts/header.php
@@ -10,7 +10,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>UR</b>C</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><i><b>UR</b>CONSULTANCY SYETEM</i>/span>
+      <span class="logo-lg"><i><b>UR</b>CONSULTANCY SYETEM</i></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
