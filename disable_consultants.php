40b962632ae64c5aafcb31889cb6fb34f6071ae4
pattyish <patrickishimwe10@gmail.com>
2020-12-23 10:04:33 +0200

    new page call disabled consultant

--- disable_consultants.php
+++ disable_consultants.php
@@ -5,17 +5,12 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-        <div class="btn-group btn-group-sm table-button-div" style="margin-top: 15px;">
-            <a href="disable_consultants.php" class=" btn btn-warning table_button">
-                Disabled Consultants
-            </a>
-            <a href="add_consultant.php" class=" btn btn-info table_button">
-                <i class="fa fa-plus"></i> Add Consultants
-            </a>
-        </div>
-        <ol class="breadcrumb" style="margin-top: 15px;">
-            <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Consultants</li>
+        <h1>
+            Consultants
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
         </ol>
     </section>
 
@@ -27,11 +22,12 @@
                 <div class="box" style="padding-left: 5px; padding-right: 5px;">
                     <div class="box-header"
                         style="border-bottom: 1px solid rgba(60, 141, 188, 0.3); margin-bottom: 10px;">
-                        <h3 class="box-title" style="padding-top: 10px; padding-bottom: 10px;">All Consultants</h3>
+                        <h3 class="box-title" style="padding-top: 10px; padding-bottom: 10px;">All Completed
+                            Consultancies</h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                        <table id="all_consultants" class="table table-bordered table-striped">
+                        <table id="completed_consultancy" class="table table-bordered table-striped">
                             <thead>
                                 <tr class="text-blue">
                                     <th>Names</th>
@@ -43,10 +39,8 @@
                                 </tr>
                             </thead>
                             <tbody>
-
-                                <?php include 'backend/existingConsultants.php'; ?>
+                                <?php include 'backend/disabled_consultants.php'; ?>
                             </tbody>
-
                         </table>
                     </div>
                 </div>
