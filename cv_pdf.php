1abec8a59346372976682f686f319f7af37462c2
pattyish <patrickishimwe10@gmail.com>
2020-12-24 22:48:02 +0200

    edit consultant form

---
+++ cv_pdf.php
@@ -0,0 +1,98 @@
+<?php 
+if(isset($_GET['cv_id'])){
+// Include the main TCPDF library (search for installation path).
+require_once('TCPDF/tcpdf.php');
+
+class PDF extends TCPDF{
+	public function Header(){
+    	$image_file = K_PATH_IMAGES.'logo_example.jpg';
+		$this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+		// Set font
+		$this->Ln(5);
+		$this->SetFont('helvetica', 'B', 20);
+		// Title
+		$this->Cell(0, 15, 'CURRICULUM VITAE ', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+		$this->Ln(15);
+		$this->Cell(0, 15, ' ', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+	}
+	public function Footer(){
+		
+	}
+}
+
+// create new PDF document
+$pdf = new PDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('TCPDF Example 001');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+$pdf->setFooterData(array(0,64,0), array(0,64,128));
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+	require_once(dirname(__FILE__).'/lang/eng.php');
+	$pdf->setLanguageArray($l);
+}
+
+// ---------------------------------------------------------
+
+// set default font subsetting mode
+$pdf->setFontSubsetting(true);
+
+// Set font
+// dejavusans is a UTF-8 Unicode font, if you only need to
+// print standard ASCII chars, you can use core fonts like
+// helvetica or times to reduce file size.
+$pdf->SetFont('dejavusans', '', 14, '', true);
+
+// Add a page
+// This method has several options, check the source code documentation for more information.
+$pdf->AddPage();
+
+// set text shadow effect
+$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+
+// Set some content to print
+$html = <<<EOD
+
+EOD;
+
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+// ---------------------------------------------------------
+
+// Close and output PDF document
+// This method has several options, check the source code documentation for more information.
+$pdf->Output('cv.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
+
+}
+?>
---
+++ edit_consultant.php
@@ -0,0 +1,147 @@
+<?php include 'public/includes/header_link.php';?>
+<?php include 'public/includes/layouts/header.php';?>
+<?php include 'public/includes/layouts/left_bar_side.php';?>
+<?php
+$consultant_id = $_GET['consultant_id'];
+// file to retrieve all existing consultants and show them in table with possible options
+$retrieve = "SELECT * FROM users INNER JOIN user_type INNER JOIN department INNER JOIN country ON 
+users.user_type_id = user_type.user_type_id AND users.user_department = department.department_id AND 
+users.user_country = country.country_id WHERE users.user_status_id = 1 AND users.user_id = $consultant_id";
+$retrieve = mysqli_query($connect,$retrieve);
+$retrieveCount = mysqli_num_rows($retrieve);
+if($retrieveCount > 0)
+{
+    while($lineRetrieve = mysqli_fetch_object($retrieve))
+    {
+        $user_id = $lineRetrieve -> user_id;
+        $fName = $lineRetrieve -> user_first_name;
+        $lName = $lineRetrieve -> user_last_name;
+        $gender = $lineRetrieve -> user_gender;
+        $natId = $lineRetrieve -> user_national_id;
+        $email = $lineRetrieve -> user_email;
+        $department = $lineRetrieve -> department_name;
+        $user_type = $lineRetrieve -> user_type;
+        $user_type_id = $lineRetrieve -> user_type_id;
+        $department_id = $lineRetrieve -> department_id;
+        $country_name = $lineRetrieve -> country_name;
+        $country_id = $lineRetrieve -> country_id;
+    }
+}        
+        ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content" style="margin-top: 20px;">
+        <!-- Main row -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Edit Consultant Info</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <form role="form" id="saveConsultant">
+                        <div class="box-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="">First-name</label>
+                                        <input type="text" value="<?php echo  $fName; ?>" class="form-control"
+                                            id="fName" placeholder="Enter First-name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Last-name</label>
+                                        <input type="text" value="<?php echo  $lName; ?>" class="form-control"
+                                            id="lName" placeholder="Enter Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Gender</label>
+                                        <select id="gender" class="form-control">
+                                            <option value="<?php echo  $gender; ?>">
+                                                <?php
+                                            if($gender == "M"){
+                                             echo "Male";
+                                            }else{
+                                             echo "Female";
+                                            }
+                                             ?>
+                                            </option>
+                                            <option value="M">Male</option>
+                                            <option value="F">Female</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">National Id &nbsp;&nbsp; <span class="text-blue" id="natIdLength">
+                                            </span></label>
+                                        <input type="text" value="<?php echo  $natId; ?>" class="form-control"
+                                            id="natId" placeholder="Enter Id Number">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input type="email" value="<?php echo  $email; ?>" class="form-control"
+                                            id="userEmail" placeholder="Enter email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">User Type </label>
+                                        <select id="userType" class="form-control">
+                                            <option value="<?php echo $user_type_id; ?>"><?php echo $user_type; ?>
+                                            </option>
+                                            <?php
+                                        include 'backend/getUserTypes.php';
+                                        ?>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Country</label>
+                                        <select class="form-control select2" id="country" multiple="multiple"
+                                            data-placeholder="Select a State" style="width: 100%;">
+                                            <option value="<?php echo  $country_id; ?>" selected>
+                                                <?php echo  $country_name; ?></option>
+                                            <?php include 'backend/getCountries.php'; ?>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Department</label>
+                                        <select class="form-control select2" id="department" multiple="multiple"
+                                            data-placeholder="Select a State" style="width: 100%;">
+                                            <option value="<?php echo $department_id; ?>" selected>
+                                                <?php echo $department; ?></option>
+                                            <?php include 'backend/getDepartments.php'; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+
+                        <div class="box-footer">
+                            <button type="submit" id="saveCons" class="btn btn-success">Update</button>
+                            &nbsp;&nbsp;&nbsp;
+                            <br><br>
+                            <span id="feedback"> </span>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.box -->
+            </div>
+        </div>
+        <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- footer -->
+<?php include "public/includes/footer.php"; ?>
