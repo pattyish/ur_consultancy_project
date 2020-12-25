dcc9a7556b3666866af615c0bd70cc4a93531a6e
Jay P <nishimirwepaul2015@gmail.com>
2020-12-25 21:29:31 +0200

    Pazzo, Look how I have changed the CV. I thought that may make sense than it was before.

--- cv_pdf.php
+++ cv_pdf.php
@@ -3,9 +3,10 @@ include 'backend/Database.php';
 if(isset($_GET['cv_id'])){
 	$consultant_id = $_GET['cv_id'];
 	// file to retrieve all existing consultants and show them in table with possible options
-	$retrieve = "SELECT * FROM users INNER JOIN user_type INNER JOIN department INNER JOIN country ON 
-	users.user_type_id = user_type.user_type_id AND users.user_department = department.department_id AND 
-	users.user_country = country.country_id WHERE users.user_status_id = 1 AND users.user_id = $consultant_id";
+	$retrieve = "SELECT * FROM users INNER JOIN user_type INNER JOIN department INNER JOIN country INNER JOIN 
+	school ON department.school_id  = school.school_id AND users.user_type_id = user_type.user_type_id AND
+	 users.user_department = department.department_id AND 	users.user_country = country.country_id
+	  WHERE users.user_status_id = 1 AND users.user_id = $consultant_id";
 	$retrieve = mysqli_query($connect,$retrieve);
 	$retrieveCount = mysqli_num_rows($retrieve);
 	$gender_value = "";
@@ -13,6 +14,7 @@ if(isset($_GET['cv_id'])){
 	$education = "";
 	$summary = "";
 	$location = "";
+	$now = date("d/m/Y");
 	if($retrieveCount > 0)
 	{
 		while($lineRetrieve = mysqli_fetch_object($retrieve))
@@ -24,6 +26,7 @@ if(isset($_GET['cv_id'])){
 			$natId = $lineRetrieve -> user_national_id;
 			$email = $lineRetrieve -> user_email;
 			$department = $lineRetrieve -> department_name;
+			$school = $lineRetrieve -> school_name;
 			$user_type = $lineRetrieve -> user_type;
 			$user_type_id = $lineRetrieve -> user_type_id;
 			$department_id = $lineRetrieve -> department_id;
@@ -33,23 +36,32 @@ if(isset($_GET['cv_id'])){
 			$user_summary = $lineRetrieve -> user_summary;
 			$user_education = $lineRetrieve -> user_education;
 			$user_location = $lineRetrieve -> user_location;
-			if($gender == "M"){
+			if($gender == "M")
+			{
               $gender_value = "Male";
-			}else{
+			}
+			else
+			{
 			  $gender_value ="Female";
 			}
-			if($user_phone == "Input your number"){
+			if($user_phone == "Input your number")
+			{
 				$phone_number = "No Updated Yet";
-			}else{
+			}
+			else
+			{
                 $phone_number = $user_phone;
 			}
-			if($user_location == ""){
+			if($user_location == "")
+			{
 				$location = "No Updated Yet";
-			}else{
+			}
+			else
+			{
 				$location = $user_location;	
 			}
 		}
-	}        	
+	}    	
 // Include the main TCPDF library (search for installation path).
 require_once('TCPDF/tcpdf.php');
 
@@ -59,9 +71,9 @@ class PDF extends TCPDF{
 		$this->Ln(5);
 		$this->SetFont('times', 'B', 16);
 		// Title
-		$this->Cell(0, 15, 'CURRICULUM VITAE ', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+		$this->Cell(0, 15, 'UR CONSULTANCY MEMBERSHIP CONFIRMATION ', 0, false, 'C', 0, '', 0, false, 'M', 'M');
 		$this->Ln(1);
-		$this->Cell(0, 15, '_________________________________________________________________ ', 0, 0, 'L');
+		//$this->Cell(0, 15, '______________________________________ ', 0, 0, 'L');
 		$this->Ln(10);
 	}
 }
@@ -71,8 +83,8 @@ $pdf = new PDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', f
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('hello');
+$pdf->SetAuthor('Paul Patrick');
+$pdf->SetTitle('My CV');
 $pdf->SetSubject('TCPDF Tutorial');
 $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
@@ -160,39 +172,26 @@ $pdf->SetFont('times', 'B', 14);
 $pdf->Cell(40,5,'Location',0,0,'L');
 $pdf->SetFont('times', '', 14);
 $pdf->Cell(149,5,$location ,0,0,'L');
-$pdf->Ln(15);
-//summary
-$pdf->SetFont('times', 'B', 16);
-$pdf->Cell(189,5,'SUMMARY',0,0,'L');
-$pdf->Ln(1);
-$pdf->Cell(189, 15, '__________', 0, 0,'L');
-$pdf->SetFont('times', '', 14);
-$pdf->Ln(15);
-$pdf->MultiCell(189,5,$user_summary,0, 'L', 0, 1, '', '', true);
-$pdf->Ln(10);
+$pdf->Ln(25);
 //education
 $pdf->SetFont('times', 'B', 16);
-$pdf->Cell(189,5,'EDUCATION BACKGROUND',0,0,'L');
+$pdf->Cell(189,5,'EDUCATION BACKGROUND AND SPECIFICATION',0,0,'L');
 $pdf->Ln(1);
 $pdf->Cell(189, 15,'___________________________', 0, 0,'L');
 $pdf->SetFont('times', '', 14);
-$pdf->Ln(15);
+$pdf->Ln(10);
 $pdf->MultiCell(189,5,$user_education,0, 'L', 0, 1, '', '', true);
 //footer
 $pdf->Ln(15);
 $pdf->SetFont('times', 'B', 16);
-$pdf->Cell(189,5,'CERTIFICATION',0,0,'L');
+$pdf->Cell(189,5,'CONFIRMATION',0,0,'L');
 $pdf->Ln(1);
 $pdf->Cell(189, 15, '________________', 0, 0,'L');
 $pdf->SetFont('times', '', 14);
 $pdf->Ln(15);
-$pdf->MultiCell(189, 15, 'I, the undersigned, here by faithfully certify, with all my knowledge, that the above mentioned Information depicts my current situation.', 0, 'L', 0, 1, '', '', true);
-$pdf->Ln(5);
-$pdf->Cell(189,5,'Done at Kigali on ___________________________',0,0,'C');
-$pdf->Ln(8);
-$pdf->Cell(189,5,$fName.' '.$lName,0,0,'C');
-$pdf->Ln(5);
-$pdf->Cell(189,5,'_______________________________',0,0,'C');
+$pdf->MultiCell(189, 15, 'This is to confirm that '.$fName.' '.$lName.' is an active consultant of University of Rwanda, in school of '.$school.' and department of '.$department.'.', 0, 'L', 0, 1, '', '', true);
+$pdf->Ln(40);
+$pdf->Cell(100,5,'Done at Kigali on    '.$now,0,0,'C');
 // set text shadow effect
 $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
 
