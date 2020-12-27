3f127f297a7557f1094552ee9aa78e34884735a4
pattyish <patrickishimwe10@gmail.com>
2020-12-27 11:16:01 +0200

    add chat model is done bu i remove data-toggler on button

--- chat_room.php
+++ chat_room.php
@@ -27,7 +27,7 @@ include 'backend/jsForChatting1.php';
             <div class="col-md-4">
                 <div class="box">
                     <div class="box-header with-border">
-                        <h3 class="box-title text-gray" id="onView"><b>Partners</b></h3>
+                        <h3 class="box-title text-gray active" id="onView"><b></b></h3>
                         <div class="box-tools pull-right">
                             <a href="#" id="viewPartners" title="Display partners to chat with">
                                 <span class=" label label-primary">View partners</span>
@@ -36,7 +36,7 @@ include 'backend/jsForChatting1.php';
                                 <span class=" label label-primary" id="viewGroups">View Groups</span>
                             </a>
                             <span class="label label-success" title="Create a new group">
-                            <a href="#" style="text-decoration: none;" data-toggle="modal"
+                                <a href="#" style="text-decoration: none;" data-toggle="modal"
                                     data-target="#create_group"> Create Group</a>
                             </span>
                         </div>
@@ -45,7 +45,7 @@ include 'backend/jsForChatting1.php';
                     <!-- /.box-header -->
                     <div class="box-body no-padding" id="groupOrIndividual">
                         <!-- /.users-list -->
-                            <?php include 'backend/getUsersToChatWith.php'; ?>
+                        <?php include 'backend/getUsersToChatWith.php'; ?>
                     </div>
                     <!-- /.box-body -->
                     <div class="box-footer text-center text-blue">
@@ -57,11 +57,14 @@ include 'backend/jsForChatting1.php';
             <div class="col-md-8">
                 <div class="box direct-chat direct-chat-primary">
                     <div class="box-header with-border">
-                        <h2 class="box-title text-green" id="chat_with">Choose a user to chat with</h2>
+                        <h2 class="box-title text-success" id="chat_with">Choose a user to chat with</h2>
 
                         <div class="box-tools pull-right">
-                            <button data-toggle="tooltip" id="addNewMemberBtn" title="Add a group member" class="badge bg-primary" disabled hidden>+</button>
+                            <button  id="addNewMemberBtn" title="Add a group member"
+                                data-toggle="modal" data-target="#add_group_member" class="badge bg-success" disabled
+                                hidden><i class="fa fa-plus"></i>Add Group Member</button>
                             &nbsp;&nbsp;&nbsp;
+                            <?php include 'models/add_group_member.php'; ?>
                             <button style="font-size: 17px;" type="button" class="btn btn-box-tool"
                                 data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
                                 <i class="fa fa-comments"></i></button>
---
+++ models/add_group_member.php
@@ -0,0 +1,34 @@
+<div class="modal fade" id="add_group_member">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"><b><span
+                            class="text-info"><b>Add Member to Group</b></span>
+                    </b></h4>
+            </div>
+            <div class="modal-body">
+                <form class="form" id="searchConsultant">
+                    <div class="form-group">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-group">
+                            <label for="">Find User By Email </label>
+                            <?php
+                            include 'backend/getUsersNatId.php';
+                            ?>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button> &nbsp;&nbsp;
+                <span style="font-size: 10px;" id=""></span> &nbsp;&nbsp;
+                <button type="button" class="btn btn-primary"><i class="fa fa-plus"></i>Add Member</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
