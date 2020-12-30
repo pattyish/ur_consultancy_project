231284c0c94254908a946dc5775beb3764056b6c
pattyish <patrickishimwe10@gmail.com>
2020-12-30 18:33:17 +0200

    changes

--- public/includes/footer_link.php
+++ public/includes/footer_link.php
@@ -34,6 +34,7 @@
 <!-- DataTables -->
 <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="bower_components/Buttons-1.6.5/js/buttons.bootstrap.min.js"></script>
 <!-- Select2 -->
 <script src="bower_components/select2/dist/js/select2.full.min.js"></script>
 <script src="plugins/iCheck/icheck.min.js"></script>
