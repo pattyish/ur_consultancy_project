61820d2d2c66351ad0938f314097e6b41b1f0ea5
pattyish <patrickishimwe10@gmail.com>
2021-01-05 09:48:06 +0200

    update notification

--- public/includes/layouts/header.php
+++ public/includes/layouts/header.php
@@ -3,20 +3,20 @@
 
     <div class="wrapper">
 
-    <header class="main-header">
-        <!-- Logo -->
-        <a href="Home" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>UR</b>C</span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><i><b>UR CONSULTANCY SYSTEM<</b></i></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-            <span class="sr-only">Toggle navigation</span>
-        </a>
+        <header class="main-header">
+            <!-- Logo -->
+            <a href="Home" class="logo">
+                <!-- mini logo for sidebar mini 50x50 pixels -->
+                <span class="logo-mini"><b>UR</b>C</span>
+                <!-- logo for regular state and mobile devices -->
+                <span class="logo-lg"><i><b>UR CONSULTANCY SYSTEM<< /b></i></span>
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                </a>
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
                         <!-- Messages: style can be found in dropdown.less-->
@@ -45,6 +45,18 @@
                                         <li>
                                             <a href="#">
                                                 <i class="fa fa-shopping-cart text-green"></i> 25 sales made
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="fa fa-warning text-yellow"></i> Very long description here
+                                                that may not fit into the
+                                                page and may cause design problems
                                             </a>
                                         </li>
                                         <li>
@@ -81,7 +93,8 @@
                                         <b>
                                             <?php 
                                                 $now = date("Y-m-d");
-                                                echo $MYFirstname; ?> - <span class="text-pink"><?php echo $MYUserTypeName; ?></span>
+                                                echo $MYFirstname; ?> - <span
+                                                class="text-pink"><?php echo $MYUserTypeName; ?></span>
                                             <br>
                                             <small>
                                                 <?php echo date("d")."-".date("m")."-".date("Y").", on ".date("D", strtotime($now)); ?>
